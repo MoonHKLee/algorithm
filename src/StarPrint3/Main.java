445b7588958355c9017769f9fa4eff7a59eb1da9
MoonHKLee <mf311110@gmail.com>
2019-04-09 21:51:29 +0900

    별1개씩 감소

--- src/StarPrint3/Main.java
+++ src/StarPrint3/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int num;
+        Scanner scan = new Scanner(System.in);
+        num=scan.nextInt();
 
+        for (int i = 0; i < num; i++) {
+            for (int j = num; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
     }
 }
