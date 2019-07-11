98ffffae9212522b64bc7977fed11163a9282c80
MoonHKLee <mf311110@gmail.com>
2019-07-11 18:51:55 +0900

    0710

---
+++ src/DoublePlus/Main.java
@@ -0,0 +1,9 @@
+package DoublePlus;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/JavaThreadTest/Main.java
@@ -0,0 +1,8 @@
+package JavaThreadTest;
+
+public class Main {
+    public static void main(String[] args) {
+        ThreadTest 스레드1 = new ThreadTest("c:어쩌구저쩌구","이문혁",20);
+        스레드1.start();
+    }
+}
