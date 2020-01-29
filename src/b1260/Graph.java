14a7c7804a83390b1bd0315a41e472b095f5a019
MoonHKLee <mf311110@gmail.com>
2020-01-29 20:02:23 +0900

    1260 백준 dfs bfs && 5585 백준 greedy

--- src/b1260/Graph.java
+++ src/b1260/Graph.java
@@ -1,17 +1,21 @@
 package b1260;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Graph {
     private int vertex;//정점의 갯수
     private int [][] arr;//입력값을 바탕으로 만들어낸 배열
     private List<Integer> adj;
+    Queue<Integer> queue;
 
     public Graph(int vertex) {
         this.vertex = vertex;
         arr = new int[this.vertex+1][this.vertex+1];//배열 생성
-        adj = new ArrayList<Integer>();
+        adj = new ArrayList<>();
+        queue = new LinkedList<>();
     }
 
     public int[][] setVertex(int vertex1,int vertex2){
@@ -29,6 +33,11 @@ public class Graph {
         }
     }
 
+    public void clear () {
+        adj.clear();
+        queue.clear();
+    }
+
     public void dfs (int start){
         adj.add(start);
         System.out.print(start+" ");
@@ -36,6 +45,23 @@ public class Graph {
             if(arr[i][start] == 1 && !adj.contains(i)){
                 dfs(i);
             }
+        }
+    }
+
+    public void bfs (int start){
+        adj.add(start);
+        if(queue.isEmpty()) {
+            queue.offer(start);
+        }
+        for (int i = 1; i < arr.length ; i++) {
+            if(arr[start][i] == 1 && !adj.contains(i) && !queue.contains(i)){
+                queue.offer(i);
+            }
+        }
+        System.out.print(queue.peek()+" ");
+        queue.poll();
+        if(!queue.isEmpty()){
+            bfs(queue.peek());
         }
     }
 }
