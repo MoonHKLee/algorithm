a3526e00f4ab1a69d04a07dac8d6f56f5bc489ec
MoonHKLee <mf311110@gmail.com>
2020-06-19 00:04:49 +0900

    0618 greedy

--- src/programmers/p03/Solution.java
+++ src/programmers/p03/Solution.java
@@ -1,23 +1,26 @@
 package programmers.p03;
 
-import java.util.Arrays;
-
 class Solution {
 
     public static void main(String[] args) {
-
+        int n = 3;
+        int[] lost = {3};
+        int[] reserve = {1};
+        Solution solution = new Solution();
+        System.out.println(solution.solution(n,lost,reserve));
     }
 
     public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
         int [] totalClothes = new int [n];
         for (int i = 0; i < n; i++) {
             totalClothes[i]=1;
         }
         for (int i : lost) {
-            totalClothes[i]--;
+            totalClothes[i-1]--;
         }
         for (int i : reserve) {
-            totalClothes[i]++;
+            totalClothes[i-1]++;
         }
 
         if(totalClothes[0]==0){
@@ -29,14 +32,32 @@ class Solution {
 
         for (int i = 1; i < n-1; i++) {
             if(totalClothes[i]==0){
-                if (totalClothes[i-1]+totalClothes[i+1]==3){
-
+                if(totalClothes[i-1]==1 && totalClothes[i+1]==2){
+                    totalClothes[i+1]--;
+                    totalClothes[i]++;
                 }
-
+                if(totalClothes[i-1]==2 && totalClothes[i+1]==1){
+                    totalClothes[i-1]--;
+                    totalClothes[i]++;
+                }
+                if(totalClothes[i-1]==2 && totalClothes[i+1]==2){
+                    totalClothes[i+1]--;
+                    totalClothes[i]++;
+                }
             }
         }
 
-        int answer = 0;
+        if(totalClothes[n-1]==0){
+            if (totalClothes[n-2]==2){
+                totalClothes[n-1]++;
+                totalClothes[n-2]--;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(totalClothes[i]!=0)
+                answer++;
+        }
         return answer;
     }
 }
